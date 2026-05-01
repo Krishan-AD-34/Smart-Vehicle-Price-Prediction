4d3fbf9ae3496f57f69e716ebe21445624add736
Krishan kumar <krishankumar@Krishans-MacBook-Air.local>
2026-05-01 21:44:45 +0530

    fixed render port

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, jsonify, render_template
 import joblib
 import numpy as np
@@ -68,5 +70,6 @@ def predict():
         })
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
